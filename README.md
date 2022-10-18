ad97aa0b0bd6f4b560d6d7aa5762d576d49596eb
acvtsjy <40293192+acvtsjy@users.noreply.github.com>
2022-10-18 15:04:17 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -2,3 +2,98 @@
 
 This is a simple fullstack random generator program 
 developed by @lorsir 
+
+## Install
+
+``` bash
+npm install
+
+``` 
+
+### Usage backend
+``` bash
+ts-node main.ts
+```
+
+## esempio output
+
+Random number
+{
+  "Bari": [
+    77,
+    18,
+    76,
+    27,
+    3
+  ],
+  "Cagliari": [
+    23,
+    20,
+    63,
+    22,
+    50
+  ],
+  "firenze": [
+    73,
+    21,
+    6,
+    43,
+    40
+  ],
+  "Genova": [
+    41,
+    72,
+    48,
+    66,
+    17
+  ],
+  "Milano": [
+    27,
+    65,
+    23,
+    52,
+    41
+  ],
+  "napoli": [
+    83,
+    46,
+    87,
+    2,
+    17
+  ],
+  "Palermo": [
+    17,
+    9,
+    84,
+    36,
+    52
+  ],
+  "Roma": [
+    2,
+    8,
+    39,
+    43,
+    38
+  ],
+  "torino": [
+    9,
+    65,
+    10,
+    8,
+    22
+  ],
+  "Venezia": [
+    4,
+    2,
+    15,
+    61,
+    47
+  ],
+  "Nazionale": [
+    68,
+    5,
+    63,
+    87,
+    62
+  ]
+}
--- main.ts
+++ main.ts
@@ -1,1 +1,56 @@
-console.log('hello world');
+import { mainModule } from "process";
+
+console.log('Random number');
+
+const randomNumber = Math.random();
+// generazione numeri pseudo randomici
+function RNGdec(min: number, max: number, precision: number)
+{
+    // codice!
+    const rng = Math.random();
+    return (rng * (max - min) + min).toFixed(precision);
+}
+function RNGdec_1(min: number, max: number, precision: number)
+{
+    // codice!
+    const multFactor = Math.pow(10,precision);    
+    return RNG(min * multFactor, max*multFactor) / multFactor;
+}
+
+function RNG(min: number, max: number) {
+    const rng = Math.random();
+    return Math.trunc((rng * (max - min) + min));
+}
+
+function RNGSequance(len: number, min: number, max: number) {
+
+    if (len >= (max-min)) {
+        throw new Error(`Cannot find ${len} numbers between ${min} and ${max}`);        
+    }
+
+    const res: number[] = [];
+
+    while (res.length < len) {
+        const rn = RNG(min, max);
+        if (res.includes(rn)){
+            continue;
+        }
+        res.push(rn);
+    }
+    return res;
+}
+
+const route = ['Bari', 'Cagliari', 'firenze', 'Genova', 'Milano', 'napoli', 'Palermo', 'Roma', 'torino', 'Venezia', 'Nazionale'];
+
+const estrazioni: {[ruota: string]: number[] } = {};
+
+
+for (const ruota of route) {
+    const estrazione = RNGSequance(5,1,90);//,1,100);
+
+    estrazioni[ruota] = estrazione;
+    
+    
+}
+console.log(JSON.stringify(estrazioni,null, 2));
+
